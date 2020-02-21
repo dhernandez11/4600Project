4081427f137a8a5f12e700dc875da2c786f92fdf
Daphne Williams <daphne88veronica@gmail.com>
2020-02-21 13:59:00 -0600

    Vision Doc

--- 4600Project/Properties/Resources.Designer.cs
+++ 4600Project/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _4600Project.Properties
-{
-
-
+namespace _4600Project.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,78 @@ namespace _4600Project.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_4600Project.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 
+        ///
+        ///VISION DOCUMENT
+        /// CSCI-4600
+        ///
+        ///
+        ///Daphne Williams &amp; David Hernandez
+        ///
+        ///
+        ///
+        ///
+        ///1 Introduction
+        ///
+        ///
+        ///1.1 Purpose
+        ///
+        ///The main purpose of this vision document is to outline the vital requirements, functions, and features of
+        ///this project. 
+        ///The focus is to create a blueprint for the program and detailing the necessary libraries, the
+        ///target users, as well as the stakeholder’s abilities.
+        ///
+        ///
+        ///1.2 Scope
+        ///
+        ///This Vision Document applies to the Calendar Project, name TBD, which will be developed by Daphne 
+        ///Williams and David He [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Vision_Document {
+            get {
+                return ResourceManager.GetString("Vision_Document", resourceCulture);
             }
         }
     }
